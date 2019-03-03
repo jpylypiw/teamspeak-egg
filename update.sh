9232740a67400b90f51a2a49497423b5b19162a6
KingJP <jochen@pylypiw.com>
2019-03-03 16:40:10 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-NEWVERSION=$(curl https://raw.githubusercontent.com/KingJP/teamspeak-egg/master/tsversion)
-CURRVERSION=$(cat version.txt)
+LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/KingJP/teamspeak-egg/master/tsversion)
+echo 'Latest TeamSpeak3 Version: '$LATEST_VERSION
+INSTALLED_VERSION=$(cat version.txt)
+echo 'Installed TeamSpeak3 Version: '$INSTALLED_VERSION
 
 if [ $NEWVERSION != $CURRVERSION ];
 then
+  echo 'Update required!'
+  echo 'downloading latest version and extracting file...'
   curl https://files.teamspeak-services.com/releases/server/$TSVERSION/teamspeak3-server_linux_amd64-$TSVERSION.tar.bz2 | tar xj --strip-components=1
+  echo 'download and extraction finished'
+  chmod +x ts3server_minimal_runscript.sh
+  echo 'permissions set.'
+  echo '' > .ts3server_license_accepted
+  echo 'accepted license'
+else
+  echo 'No update required.'
 fi
 
+echo 'starting server...'
+./ts3server_minimal_runscript.sh inifile=ts3server.ini
